d5b6e2d400d8cea1233563abfd4ac79da7fb3c1c
Sujal Rai <sujal.rai147@gmail.com>
2026-09-08 20:29:19 +0545

    first commit

---
+++ app/(root)/(tabs)/_layout.jsx
@@ -0,0 +1,28 @@
+import { NativeTabs } from "expo-router/unstable-native-tabs";
+
+export default function TabsLayout() {
+  return (
+    <NativeTabs>
+      <NativeTabs.Trigger name="index">
+        <NativeTabs.Trigger.Label>Home</NativeTabs.Trigger.Label>
+        <NativeTabs.Trigger.Icon sf="house.fill" md="home" />
+      </NativeTabs.Trigger>
+
+      <NativeTabs.Trigger name="market">
+        <NativeTabs.Trigger.Icon sf="storefront.fill" md="storefront" />
+        <NativeTabs.Trigger.Label>Market</NativeTabs.Trigger.Label>
+      </NativeTabs.Trigger>
+
+      <NativeTabs.Trigger name="profile">
+        <NativeTabs.Trigger.Icon sf="person.fill" md="person" />
+        <NativeTabs.Trigger.Label>Profile</NativeTabs.Trigger.Label>
+      </NativeTabs.Trigger>
+
+      <NativeTabs.Trigger name="settings">
+        <NativeTabs.Trigger.Icon sf="gear" md="settings" />
+        <NativeTabs.Trigger.Label>Settings</NativeTabs.Trigger.Label>
+      </NativeTabs.Trigger>
+      
+    </NativeTabs>
+  );
+}
